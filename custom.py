7f05caf973f892f069b2055b552d5937953f7e64
모현정 <165883958+mohyunjeong@users.noreply.github.com>
2024-06-19 14:28:21 +0900

    Add files via upload

---
+++ custom.py
@@ -0,0 +1,30 @@
+# custom.py -> 
+
+from jetbot import Robot
+
+robot = Robot()
+
+def forward():
+    robot.left_motor.value = 0.5 # 0 ~ 1
+    robot.right_motor.value = 0.5
+    
+def backward():
+    robot.left_motor.value = -0.5
+    robot.right_motor.value = -0.5
+    
+def left():
+    robot.left_motor.value = 0.5
+    robot.right_motor.value =1.0
+    
+def right():
+    robot.left_motor.value = 1.0
+    robot.right_motor.value =0.5
+    
+def stop():
+    robot.left_motor.value = 0.0
+    robot.right_motor.value = 0.0
+    
+print("hello")
+
+if __name__ == "__main__" :
+    print("start")
